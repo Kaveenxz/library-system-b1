c219b467cef2e94094d2d279dd331a929e91b6de
Kaveenxz <kaveenhansithx@gmail.com>
2024-05-04 20:29:28 +0530

    create yml file

--- src/main/java/org/example/service/BookService.java
+++ src/main/java/org/example/service/BookService.java
@@ -1,7 +1,9 @@
 package org.example.service;
 
 import org.example.dto.Book;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface BookService {
     void addBook(Book book);
 }
